a3b99249c4477d52514820e976f25c5cea16d228
Matt McClard <matt@groupswitch.com>
2014-02-02 14:57:55 -0600

    Wired Collection into clientsList

--- client/views/clients/clients_list.js
+++ client/views/clients/clients_list.js
@@ -1,15 +1,5 @@
-var clientsData = [
-	{
-		title: "Maggiano's Little Italy"
-	},
-	{
-		title: "Cultivar Coffee"
-	},
-	{
-		title: "Boxer & Street"
-	}
-];
-
 Template.clientsList.helpers({
-	clients: clientsData
+	clients: function(){
+		return Clients.find();
+	}
 });
