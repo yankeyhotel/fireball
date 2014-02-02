e5266aa4b1f3599c1feb06ddeb6c693b16f9aa1c
Matt McClard <matt@groupswitch.com>
2014-02-01 22:47:52 -0600

    Set up and first commit.

---
+++ client/views/clients/clients_list.js
@@ -0,0 +1,15 @@
+var clientsData = [
+	{
+		title: "Maggiano's Little Italy"
+	},
+	{
+		title: "Cultivar Coffee"
+	},
+	{
+		title: "Boxer & Street"
+	}
+];
+
+Template.clientsList.helpers({
+	clients: clientsData
+});
