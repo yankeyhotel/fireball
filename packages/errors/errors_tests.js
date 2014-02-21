f2836ce758cb98d0472312e26c2cde2017598c10
Matt McClard <yankeyhotel@gmail.com>
2014-02-21 14:59:03 -0600

    Finished Errors Package

--- packages/errors/errors_tests.js
+++ packages/errors/errors_tests.js
@@ -19,7 +19,7 @@ Tinytest.addAsync("Errors template works", function(test, done) {
 
 	// wait a few milliseconds
 	Meteor.setTimeout(function() {
-		test.equal(Errors.collection.find({seen: true}).count(), 0);
+		test.equal(Errors.collection.find({seen: false}).count(), 0);
 		test.equal(Errors.collection.find({}).count(), 1);
 		Errors.clearSeen();
 
