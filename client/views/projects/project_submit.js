3cfcc9dca33e76d5408cfe66078eb270a08df39e
Matt McClard <yankeyhotel@gmail.com>
2014-02-24 23:43:16 -0600

    Added Bootstrap DatePicker, Dynamically pull in team members into create new project form

--- client/views/projects/project_submit.js
+++ client/views/projects/project_submit.js
@@ -1,3 +1,15 @@
+Template.projectSubmit.rendered = function() {
+    $('.datetimepicker').datetimepicker();
+}
+
+Template.projectSubmit.helpers({
+	userByRole: function (role) {
+		// role = "Project Manager";
+		// console.log( Meteor.users.find({}, { fields: { 'profile.role': 1, _id: 0 } }).fetch() );
+		return Meteor.users.find({ "profile.role" : role });
+	}
+});
+
 Template.projectSubmit.events ({
 
 	'submit form': function(e, template) {
@@ -5,10 +17,11 @@ Template.projectSubmit.events ({
 		e.preventDefault();
 
 		var $title = $(e.target).find('[name=title]');
+
 		var project = {
 			title: $title.val(),
 			status: $(e.target).find('[name=status]:checked').val(),
-			clientId: template.data._id
+			clientId: this._id
 		};
 
 		Meteor.call('project', project, function(error, projectId) {
@@ -23,4 +36,20 @@ Template.projectSubmit.events ({
 
 	}
 
-})
+});
+
+
+
+Template.userByRole.helpers({
+
+	setName: function(role) {
+		if (role == "Project Manager") {
+			return "projectManagers";
+		} else if (role == "Designer") {
+			return "designers";
+		} else if (role == "Web Developer") {
+			return "webDevelopers"
+		}
+	}
+	
+});
